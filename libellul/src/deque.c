d6db50bc64a319e265d516d9853efcafc7754b67
Maxime <maxime.merigot--drucy@phelma.grenoble-inp.fr>
2025-10-03 11:46:14 +0200

    feat deque.c

--- libellul/src/deque.c
+++ libellul/src/deque.c
@@ -10,35 +10,34 @@ typedef struct {
     void            *datum;
 } link_t;
 
-void *dequeue_new__(){
+void *deque_new__(){
+    // TO DO
+    return NULL;
+}
+
+void deque_delete__(void *queue){
     // TO DO
 }
 
-void dequeue_delete__(void *queue){
+int deque_is_empty__(void *queue){
+    // TO DO
+
+    return deque_new__() == queue;
+}
+
+void deque_push_front__(void *queue, void *element){
     // TO DO
 }
 
-size_t dequeue_size__(void *queue){
+void deque_push_back__(void *queue, void *element){
     // TO DO
 }
 
-int dequeue_is_empty__(void *queue){
+void *deque_pop_front__(void *queue){
     // TO DO
 }
 
-void dequeue_push_front__(void *queue, void *element){
-    // TO DO
-}
-
-void dequeue_push_back__(void *queue, void *element){
-    // TO DO
-}
-
-void *dequeue_pop_front__(void *queue){
-    // TO DO
-}
-
-void *dequeue_pop_back__(void *queue){
+void *deque_pop_back__(void *queue){
     // TO DO
 }
 

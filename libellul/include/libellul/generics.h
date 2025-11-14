92b29d66ddb9c97f60e0f0572e66b471c57e3b46
BOUCHUT Tanguy <tanguy.bouchut@phelma.grenoble-inp.fr>
2025-11-14 11:22:31 +0100

    FEAT ajout de datum_t en paramètre

--- libellul/include/libellul/generics.h
+++ libellul/include/libellul/generics.h
@@ -3,7 +3,6 @@
 
 #define GEN_CONCAT( foo, bar ) foo ## bar
 #define GEN_CAT( foo, bar )    GEN_CONCAT( foo, bar )
-
 #define GEN_SYM( foo, bar )    GEN_CAT( foo, GEN_CAT( _, bar ) )
 
 #endif
--- libellul/include/libellul/interface.h
+++ libellul/include/libellul/interface.h
@@ -3,16 +3,8 @@
 
 #include "generics.h"
 
-#ifndef T
-    #error "Undefined data structure !"
-#endif
-
-#ifndef datum_t
-    #error "Undefined datum type !"
-#endif
-
 #define TYPENAME(T, datum_t)    GEN_SYM(T, datum_t)
 #define TYPE(T, datum_t)        GEN_SYM(TYPENAME(T, datum_t), t)
-#define METHOD(T, name)            GEN_SYM(TYPENAME(T, datum_t), name)
+#define METHOD(T, datum_t, name)            GEN_SYM(TYPENAME(T, datum_t), name)
 
 #endif

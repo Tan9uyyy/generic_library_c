6d17418aca56c5b7132cd5f5d08ade41ed027921
Maxime MD <maxime.merigot-drucy@phelma.grenoble-inp.fr>
2025-12-02 18:49:13 +0100

    FIX use of b tree

--- test/props/07-b-tree.c
+++ test/props/07-b-tree.c
@@ -10,15 +10,15 @@ void printer(int value){printf("%d", value);}
 int comparator(int v1, int v2){return v1 - v2;}
 
 void tree_NULL( void ) {
-    binary_tree_int_t tree = binary_tree_int_new();
+    b_tree_int_t tree = b_tree_int_new();
 
-    binary_tree_int_print(tree, printer); printf("\n");
+    b_tree_int_print(tree, printer); printf("\n");
 
     test_suite( "NULL as a tree" );
 
-    test_assert( binary_tree_int_is_empty( tree ), "NULL is an empty tree" );
+    test_assert( b_tree_int_is_empty( tree ), "NULL is an empty tree" );
 
-    binary_tree_int_delete( tree, NULL );
+    b_tree_int_delete( tree, NULL );
     test_assert( 1, "Can delete NULL as a tree" );
 
     free(tree);
@@ -27,43 +27,43 @@ void tree_NULL( void ) {
 void tree_tests(){
     test_suite("Tree push one element");
 
-    binary_tree_int_t tree = binary_tree_int_new();
+    b_tree_int_t tree = b_tree_int_new();
 
-    binary_tree_int_print(tree, printer); printf("\n");
+    b_tree_int_print(tree, printer); printf("\n");
 
     int root = 42; int big = 102; int small = 6;
-    tree = binary_tree_int_push(tree, root, comparator);
+    tree = b_tree_int_push(tree, root, comparator);
 
-    binary_tree_int_print(tree, printer); printf("\n");
+    b_tree_int_print(tree, printer); printf("\n");
 
-    test_assert(!binary_tree_int_is_empty(tree), "Tree is not empty after push");
+    test_assert(!b_tree_int_is_empty(tree), "Tree is not empty after push");
     test_assert(tree->value == root, "Root is the expected value");
-    test_assert(binary_tree_int_contains(tree, root, comparator), "Tree contains root");
+    test_assert(b_tree_int_contains(tree, root, comparator), "Tree contains root");
 
     test_suite("Tree push multiple elements");
 
-    tree = binary_tree_int_push(tree, big, comparator);
-    tree = binary_tree_int_push(tree, small, comparator);
+    tree = b_tree_int_push(tree, big, comparator);
+    tree = b_tree_int_push(tree, small, comparator);
 
-    binary_tree_int_print(tree, printer); printf("\n");
+    b_tree_int_print(tree, printer); printf("\n");
 
     test_assert(tree->ls->value == small, "Left is the expected value");
-    test_assert(binary_tree_int_contains(tree, small, comparator), "Tree contains small");
+    test_assert(b_tree_int_contains(tree, small, comparator), "Tree contains small");
 
     test_assert(tree->rs->value == big, "Right is the expected value");
-    test_assert(binary_tree_int_contains(tree, big, comparator), "Tree contains big");
+    test_assert(b_tree_int_contains(tree, big, comparator), "Tree contains big");
 
     test_suite("Tree pop small");
 
     int res_small; int res_big; int res_mid;
-    tree = binary_tree_int_pop_small(tree, &res_small);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_pop_small(tree, &res_small);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_pop_small(tree, &res_mid);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_pop_small(tree, &res_mid);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_pop_small(tree, &res_big);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_pop_small(tree, &res_big);
+    b_tree_int_print(tree, printer); printf("\n");
 
     test_assert(res_small == small, "Small value is correctly popped");
     test_assert(res_mid == root, "Mid value is correctly popped");
@@ -71,18 +71,18 @@ void tree_tests(){
 
     test_suite("Tree pop big");
 
-    tree = binary_tree_int_push(tree, small, comparator);
-    tree = binary_tree_int_push(tree, root, comparator);
-    tree = binary_tree_int_push(tree, big, comparator);
+    tree = b_tree_int_push(tree, small, comparator);
+    tree = b_tree_int_push(tree, root, comparator);
+    tree = b_tree_int_push(tree, big, comparator);
 
-    tree = binary_tree_int_pop_big(tree, &res_big);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_pop_big(tree, &res_big);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_pop_big(tree, &res_mid);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_pop_big(tree, &res_mid);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_pop_big(tree, &res_small);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_pop_big(tree, &res_small);
+    b_tree_int_print(tree, printer); printf("\n");
 
     test_assert(res_small == small, "Small value is correctly popped");
     test_assert(res_mid == root, "Mid value is correctly popped");
@@ -90,18 +90,18 @@ void tree_tests(){
 
     test_suite("Tree delete");
 
-    tree = binary_tree_int_push(tree, small, comparator);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_push(tree, small, comparator);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_push(tree, big, comparator);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_push(tree, big, comparator);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_push(tree, root, comparator);
-    binary_tree_int_print(tree, printer); printf("\n");
+    tree = b_tree_int_push(tree, root, comparator);
+    b_tree_int_print(tree, printer); printf("\n");
 
-    tree = binary_tree_int_delete(tree, NULL);
+    tree = b_tree_int_delete(tree, NULL);
 
-    test_assert(binary_tree_int_is_empty(tree), "Deleted tree is empty");
+    test_assert(b_tree_int_is_empty(tree), "Deleted tree is empty");
 
     free(tree);
 }

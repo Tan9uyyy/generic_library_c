fb4482be9eaad0d638486c90ed01d51b3833d2f6
Maxime MD <maxime.merigot-drucy@phelma.grenoble-inp.fr>
2025-12-04 16:02:36 +0100

    remove useless files

--- libellul/src/heap.c
+++
@@ -1,4 +0,0 @@
-#include <../include/libellul/type/heap.h>
-
-#define heap_datum_t int
-#include <../include/libellul/type/heap/heap-implementation.h>
--- libellul/src/s-tree.c
+++
@@ -1,4 +0,0 @@
-// #include <../include/libellul/type/s-tree.h>
-
-// #define s_tree_datum_t int
-// #include <../include/libellul/type/s-tree/s-tree-implementation.h>
